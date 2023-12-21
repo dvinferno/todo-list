7b63933a774f04e229d35a9ec273253b5982ea22
dvinferno <72324421+dvinferno@users.noreply.github.com>
2023-12-21 17:15:09 -0500

    added setCookie and getCookie for saving todo list items

--- client/src/components/Container.jsx
+++ client/src/components/Container.jsx
@@ -6,17 +6,38 @@ import { useState } from "react";
 function Container() {
     const [todoItems, setTodoItems] = useState([]);
 
+    const setCookie = (name, value, days) => {
+        const expires = new Date();
+        expires.setTime(expires.getTime() + days * 24 * 60 * 60 * 1000);
+        document.cookie = `${name}=${value};expires=${expires.toUTCString()};path=/`;
+    };
+
+    const getCookie = (name) => {
+        const cookieString = document.cookie.split('; ');
+        for (const cookie of cookieString) {
+            const [cookieName, cookieValue] = cookie.split('=');
+            if (cookieName === name) {
+                return cookieValue;
+            }
+        }
+        return null;
+    };
+
     const handleRemove = (index) => {
         setTodoItems((prevState) => {
-            const newArray = prevState.filter((item, i) => i !== index)
-            return newArray
-        })
-  };
+            const newArray = prevState.filter((item, i) => i !== index);
+            return newArray;
+        });
+    };
 
     const handleSubmit = (todoItem) => {
-        const newArray = todoItems.concat(todoItem)
-        setTodoItems(newArray)
-    }
+        const newArray = todoItems.concat(todoItem);
+        setTodoItems(newArray);
+
+        const data = JSON.stringify(newArray)
+        console.log(data)
+
+    };
 
     return (
         <div className="container">
