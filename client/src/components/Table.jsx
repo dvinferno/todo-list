feb9fabff54e2e84f020fc397dabdf3ddbb70de3
dvinferno <72324421+dvinferno@users.noreply.github.com>
2023-12-22 22:03:05 -0500

    added strikethrough effect when checkbox is toggled

--- client/src/components/Table.jsx
+++ client/src/components/Table.jsx
@@ -1,14 +1,28 @@
 /* eslint-disable react/prop-types */
 import IconButton from "./IconButton";
+function updateStatus(i, e) {
+  const row = document.getElementById(i)
+  const td = row.querySelector("#listItem")
+
+  const content = td.innerHTML
+
+  if (e.target.checked) {
+    td.innerHTML = "<s>" + content + "</s>"
+  } else {
+    td.innerHTML = td.querySelector("s").innerHTML
+  }
+  console.log(td)
+}
+
 function TableBody(props) {
   const rows = props.todoListItems.map((row, index) => {
     return (
-      <tr key={index}>
-        <td>{row}</td>
+      <tr key={index} id={index}>
+        <td id="listItem">{row}</td>
         <td>
-          <span className="edit-button">
-            <IconButton icon="fa-pen-to-square" onClick={() => {}} />
-          </span>
+          <label className="checkbox-container">
+            <input type="checkbox" className="checkbox" onChange={(e) => updateStatus(index, e)} />
+          </label>
         </td>
         <td>
           <span className="delete-button">
